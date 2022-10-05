ca22f561eb4724fe1c27503f2ff991ef65746ee9
kenneth-wang <kennethwang@aisingapore.org>
2022-10-05 16:26:34 +0800

    add more policies for different users

--- policies/general.sql
+++ policies/general.sql
@@ -85,6 +85,10 @@ CREATE OR REPLACE PACKAGE BODY USER_PACKAGE AS
                 DBMS_SESSION.SET_CONTEXT('user_ctx', 'policy_group', 'AGENT_GROUP');
             WHEN 'customer' THEN
                 DBMS_SESSION.SET_CONTEXT('user_ctx', 'policy_group', 'CUSTOMER_GROUP');
+            WHEN 'financer' THEN
+                DBMS_SESSION.SET_CONTEXT('user_ctx', 'policy_group', 'FINANCER_GROUP');
+            WHEN 'planner' THEN
+                DBMS_SESSION.SET_CONTEXT('user_ctx', 'policy_group', 'PLANNER_GROUP');
         END CASE;
     END SET_USER_CONTEXT;
 END;
@@ -101,8 +105,75 @@ BEGIN
 END;
 /
 
-GRANT EXECUTE ON USER_PACKAGE TO SFREEMAN;
+GRANT EXECUTE ON USER_PACKAGE TO SFREEMAN, CKLEIN, MCURTIS, JHUNTER, KJACKSON, ECARPENTER, AMYERS;
+GRANT EXECUTE ON USER_PACKAGE TO AARONAUSTIN, CASTILLOMICHAEL, SOSAKAITLYN, COREYSKINNER, KRISTEN73;
+GRANT EXECUTE ON USER_PACKAGE TO ROGEROLIVER, TNGUYEN, JOSHUATHOMPSON, BRENDA96, UKOCH;
+GRANT EXECUTE ON USER_PACKAGE TO STACEYGONZALEZ, SAUNDERSROGER, MONTOYAMARTIN, WEBERJOSEPH, KATHRYNFOX;
 
+
+-- -------------------------------------------------------------
+-- Create policy group for "Plan" table
+-- -------------------------------------------------------------
+
+-- Customer
+CREATE OR REPLACE FUNCTION CUSTOMER_PLAN(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    IF LOWER(SYS_CONTEXT('USERENV','CLIENT_IDENTIFIER')) = 'customer' 
+        THEN PRED := 'plan_id in (select plan_id from system.purchase)';
+    ELSE NULL;
+    END IF;
+    RETURN PRED;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_GROUPED_POLICY(
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'plan',
+        POLICY_GROUP => 'customer_group',
+        POLICY_NAME => 'filter_plan',
+        POLICY_FUNCTION => 'customer_plan',
+        STATEMENT_TYPES => 'select',
+        POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
+    );
+END;
+/
+
+-- Planner
+CREATE OR REPLACE FUNCTION PLANNER_PLAN(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    IF LOWER(SYS_CONTEXT('USERENV','CLIENT_IDENTIFIER')) = 'planner' 
+        THEN PRED := 'plan_created_by = SYS_CONTEXT(''planner_ctx'', ''planner_id'')';
+    ELSE NULL;
+    END IF;
+    RETURN PRED;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_GROUPED_POLICY(
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'plan',
+        POLICY_GROUP => 'planner_group',
+        POLICY_NAME => 'filter_plan',
+        POLICY_FUNCTION => 'planner_plan',
+        STATEMENT_TYPES => 'update',
+        POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
+    );
+END;
+/
 
 
 -- -------------------------------------------------------------
@@ -110,40 +181,21 @@ GRANT EXECUTE ON USER_PACKAGE TO SFREEMAN;
 -- -------------------------------------------------------------
 
 
--- Agent's customer policy function
+-- Agent
 CREATE OR REPLACE FUNCTION AGENT_CUSTOMER(
     SCHEMA_P IN VARCHAR2,
     TABLE_P IN VARCHAR2
 ) RETURN VARCHAR2 AS
-    CUSTOMER_PRED VARCHAR2 (400);
+    PRED VARCHAR2 (400);
 BEGIN
     IF LOWER(SYS_CONTEXT('USERENV','CLIENT_IDENTIFIER')) = 'agent' 
-        THEN CUSTOMER_PRED := 'agent_id = SYS_CONTEXT(''agent_ctx'', ''agent_id'')';
+        THEN PRED := 'agent_id = SYS_CONTEXT(''agent_ctx'', ''agent_id'')';
     ELSE NULL;
     END IF;
-    RETURN CUSTOMER_PRED;
+    RETURN PRED;
 END;
 /
 
--- Customer's customer policy function
-CREATE OR REPLACE FUNCTION CUSTOMER_CUSTOMER(
-    SCHEMA_P IN VARCHAR2,
-    TABLE_P IN VARCHAR2
-) RETURN VARCHAR2 AS
-    CUSTOMER_PRED VARCHAR2 (400);
-BEGIN
-    IF LOWER(SYS_CONTEXT('USERENV','CLIENT_IDENTIFIER')) = 'customer' 
-        THEN CUSTOMER_PRED := 'customer_id = SYS_CONTEXT(''customer_ctx'', ''customer_id'')';
-    ELSE NULL;
-    END IF;
-    RETURN CUSTOMER_PRED;
-END;
-/
-
-
-
-
--- agent_customer policy group
 BEGIN
     DBMS_RLS.ADD_GROUPED_POLICY(
         OBJECT_SCHEMA => 'system',
@@ -159,7 +211,21 @@ BEGIN
 END;
 /
 
--- customer_customer policy group
+-- Customer
+CREATE OR REPLACE FUNCTION CUSTOMER_CUSTOMER(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    IF LOWER(SYS_CONTEXT('USERENV','CLIENT_IDENTIFIER')) = 'customer' 
+        THEN PRED := 'customer_id = SYS_CONTEXT(''customer_ctx'', ''customer_id'')';
+    ELSE NULL;
+    END IF;
+    RETURN PRED;
+END;
+/
+
 BEGIN
     DBMS_RLS.ADD_GROUPED_POLICY(
         OBJECT_SCHEMA => 'system',
@@ -181,122 +247,260 @@ END;
 -- Create policy group for "Payment" table
 -- -------------------------------------------------------------
 
--- Payment policy function
+-- Agent
 CREATE OR REPLACE FUNCTION AGENT_PAYMENT(
     SCHEMA_P IN VARCHAR2,
     TABLE_P IN VARCHAR2
 ) RETURN VARCHAR2 AS
-    PAYMENT_PRED VARCHAR2 (400);
+    PRED VARCHAR2 (400);
 BEGIN
-    PAYMENT_PRED := 'payment_id in (select payment_method_id from system.customer)';
-    RETURN PAYMENT_PRED;
+    PRED := 'payment_id in (select payment_method_id from system.customer)';
+    RETURN PRED;
 END;
 /
 
 BEGIN
-    DBMS_RLS.ADD_POLICY (
+    DBMS_RLS.ADD_GROUPED_POLICY(
         OBJECT_SCHEMA => 'system',
         OBJECT_NAME => 'payment',
-        POLICY_NAME => 'agent_payment_policy',
+        POLICY_GROUP => 'agent_group',
+        POLICY_NAME => 'filter_payment',
         POLICY_FUNCTION => 'agent_payment',
         STATEMENT_TYPES => 'select',
         POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
-        NAMESPACE => 'agent_ctx',
-        ATTRIBUTE => 'agent_id'
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
     );
 END;
 /
 
+-- Customer
+CREATE OR REPLACE FUNCTION CUSTOMER_PAYMENT(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    PRED := 'payment_id in (select payment_method_id from system.customer)';
+    RETURN PRED;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_GROUPED_POLICY(
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'payment',
+        POLICY_GROUP => 'customer_group',
+        POLICY_NAME => 'filter_payment',
+        POLICY_FUNCTION => 'customer_payment',
+        STATEMENT_TYPES => 'select,update',
+        POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
+    );
+END;
+/
 
 
 -- -------------------------------------------------------------
 -- Create policy group for "Premium" table
 -- -------------------------------------------------------------
 
--- Premium policy function
+-- Agent
 CREATE OR REPLACE FUNCTION AGENT_PREMIUM(
     SCHEMA_P IN VARCHAR2,
     TABLE_P IN VARCHAR2
 ) RETURN VARCHAR2 AS
-    PREMIUM_PRED VARCHAR2 (400);
+    PRED VARCHAR2 (400);
 BEGIN
-    PREMIUM_PRED := 'customer_id in (select customer_id from system.customer)';
-    RETURN PREMIUM_PRED;
+    PRED := 'customer_id in (select customer_id from system.customer)';
+    RETURN PRED;
 END;
 /
 
 BEGIN
-    DBMS_RLS.ADD_POLICY (
+    DBMS_RLS.ADD_GROUPED_POLICY(
         OBJECT_SCHEMA => 'system',
         OBJECT_NAME => 'premium',
-        POLICY_NAME => 'agent_premium_policy',
+        POLICY_GROUP => 'agent_group',
+        POLICY_NAME => 'filter_premium',
         POLICY_FUNCTION => 'agent_premium',
         STATEMENT_TYPES => 'select',
         POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
-        NAMESPACE => 'agent_ctx',
-        ATTRIBUTE => 'agent_id'
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
     );
 END;
 /
 
+-- Customer
+CREATE OR REPLACE FUNCTION CUSTOMER_PREMIUM(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    PRED := 'customer_id in (select customer_id from system.customer)';
+    RETURN PRED;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_GROUPED_POLICY(
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'premium',
+        POLICY_GROUP => 'customer_group',
+        POLICY_NAME => 'filter_premium',
+        POLICY_FUNCTION => 'customer_premium',
+        STATEMENT_TYPES => 'select',
+        POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
+    );
+END;
+/
 
 -- -------------------------------------------------------------
 -- Create policy group for "Purchase" table
 -- -------------------------------------------------------------
 
--- Purchase policy function
+-- Agent
 CREATE OR REPLACE FUNCTION AGENT_PURCHASE(
     SCHEMA_P IN VARCHAR2,
     TABLE_P IN VARCHAR2
 ) RETURN VARCHAR2 AS
-    PURCHASE_PRED VARCHAR2 (400);
+    PRED VARCHAR2 (400);
 BEGIN
-    PURCHASE_PRED := 'customer_id in (select customer_id from system.customer)';
-    RETURN PURCHASE_PRED;
+    PRED := 'customer_id in (select customer_id from system.customer)';
+    RETURN PRED;
 END;
 /
 
 BEGIN
-    DBMS_RLS.ADD_POLICY (
+    DBMS_RLS.ADD_GROUPED_POLICY(
         OBJECT_SCHEMA => 'system',
         OBJECT_NAME => 'purchase',
-        POLICY_NAME => 'agent_purchase_policy',
+        POLICY_GROUP => 'agent_group',
+        POLICY_NAME => 'filter_purchase',
         POLICY_FUNCTION => 'agent_purchase',
         STATEMENT_TYPES => 'select',
         POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
-        NAMESPACE => 'agent_ctx',
-        ATTRIBUTE => 'agent_id'
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
     );
 END;
 /
 
+-- Customer
+CREATE OR REPLACE FUNCTION CUSTOMER_PURCHASE(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    PRED := 'customer_id in (select customer_id from system.customer)';
+    RETURN PRED;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_GROUPED_POLICY(
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'purchase',
+        POLICY_GROUP => 'customer_group',
+        POLICY_NAME => 'filter_purchase',
+        POLICY_FUNCTION => 'customer_purchase',
+        STATEMENT_TYPES => 'select',
+        POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
+    );
+END;
+/
 
 -- -------------------------------------------------------------
 -- Create policy group for "Claim" table
 -- -------------------------------------------------------------
 
--- Claim policy function
+-- Agent
 CREATE OR REPLACE FUNCTION AGENT_CLAIM(
     SCHEMA_P IN VARCHAR2,
     TABLE_P IN VARCHAR2
 ) RETURN VARCHAR2 AS
-    CLAIM_PRED VARCHAR2 (400);
+    PRED VARCHAR2 (400);
 BEGIN
-    CLAIM_PRED := 'claim_submitted_for in (select customer_id from system.customer)';
-    RETURN CLAIM_PRED;
+    PRED := 'claim_submitted_for in (select customer_id from system.customer)';
+    RETURN PRED;
 END;
 /
 
 BEGIN
-    DBMS_RLS.ADD_POLICY (
+    DBMS_RLS.ADD_GROUPED_POLICY(
         OBJECT_SCHEMA => 'system',
         OBJECT_NAME => 'claim',
-        POLICY_NAME => 'agent_claim_policy',
+        POLICY_GROUP => 'agent_group',
+        POLICY_NAME => 'filter_claim',
         POLICY_FUNCTION => 'agent_claim',
         STATEMENT_TYPES => 'select',
         POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
-        NAMESPACE => 'agent_ctx',
-        ATTRIBUTE => 'agent_id'
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
+    );
+END;
+/
+
+-- Customer
+CREATE OR REPLACE FUNCTION CUSTOMER_CLAIM(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    PRED := 'claim_submitted_for in (select customer_id from system.customer)';
+    RETURN PRED;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_GROUPED_POLICY(
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'claim',
+        POLICY_GROUP => 'customer_group',
+        POLICY_NAME => 'filter_claim',
+        POLICY_FUNCTION => 'customer_claim',
+        STATEMENT_TYPES => 'select',
+        POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
+    );
+END;
+/
+
+
+
+-- Financer
+CREATE OR REPLACE FUNCTION FINANCER_CLAIM(
+    SCHEMA_P IN VARCHAR2,
+    TABLE_P IN VARCHAR2
+) RETURN VARCHAR2 AS
+    PRED VARCHAR2 (400);
+BEGIN
+    PRED := 'claims_processed_by SYS_CONTEXT(''financer_ctx'', ''financer_id'')';
+    RETURN PRED;
+END;
+/
+
+BEGIN
+    DBMS_RLS.ADD_GROUPED_POLICY(
+        OBJECT_SCHEMA => 'system',
+        OBJECT_NAME => 'claim',
+        POLICY_GROUP => 'financer_group',
+        POLICY_NAME => 'filter_claim',
+        POLICY_FUNCTION => 'financer_claim',
+        STATEMENT_TYPES => 'select',
+        POLICY_TYPE => DBMS_RLS.CONTEXT_SENSITIVE,
+        NAMESPACE => 'user_ctx',
+        ATTRIBUTE => 'user_group'
     );
 END;
 /
